9e16d41eb01001c127cce67de53aed916fee648d
Kien Truong <duckientruong@gmail.com>
2023-03-18 11:54:38 +0700

    fix: fix envsubst installation in ubuntu base image

--- bases/ubuntu/scripts/fetch_envsubst.sh
+++ bases/ubuntu/scripts/fetch_envsubst.sh
@@ -7,7 +7,7 @@ ENVSUBST_VERSION=$1
 if [ "$TARGETPLATFORM" = "linux/arm64" ]; then
     export ENVSUBST_URL="https://github.com/a8m/envsubst/releases/download/${ENVSUBST_VERSION}/envsubst-Linux-arm64"
 elif [ "$TARGETPLATFORM" = "linux/amd64" ]; then \
-    export ENVSUBST_URL="https://github.com/a8m/envsubst/releases/download/${ENVSUBST_VERSION}/envsubst-Linux-amd64"
+    export ENVSUBST_URL="https://github.com/a8m/envsubst/releases/download/${ENVSUBST_VERSION}/envsubst-Linux-x86_64"
 fi
 
-curl "${ENVSUBST_URL}" --output /usr/local/bin/envsubst
+exec curl -L "${ENVSUBST_URL}" --output /usr/local/bin/envsubst
